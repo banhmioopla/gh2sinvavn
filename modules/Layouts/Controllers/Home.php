f4a58dc1849307e05d1ace0593f616c70a4766f9
banhmioopla <qbingking@gmail.com>
2023-06-09 00:33:29 +0700

    fix

--- modules/Layouts/Controllers/Home.php
+++ modules/Layouts/Controllers/Home.php
@@ -25,7 +25,7 @@ class Home extends BaseController
         if(is_login()){
             $account_id = session()->get('auth_data')?->account_id;
             $contracts = $this->LibUser->getContract($account_id);
-            $progress = $this->LibUser->contractCountProgress(180);
+            $progress = $this->LibUser->contractCountProgress(count($contracts));
             $contract_table = $this->LibUser->renderContractTable($account_id);
 
             return view('\Modules\Layouts\Views\home\index',[
